9eb79f2f455c33191e977390c3dfffd84997a231
Premsagarvibhute <123161189+Premsagarvibhute@users.noreply.github.com>
2023-01-25 13:30:50 +0530

    Add files via upload

---
+++ practical/Increment.java
@@ -0,0 +1,21 @@
+class Increment{
+public static void main(String args[]){
+int x=10;
+System.out.println(x++); //10
+		System.out.println(x++); //10
+		System.out.println(++x); //11
+		System.out.println(x++); //13
+		System.out.println(x);
+System.out.println(x--);
+		System.out.println(x--);
+System.out.println(x);
+             System.out.println(x--);
+System.out.println(x);
+		System.out.println(--x);
+System.out.println(x);
+		System.out.println(--x);
+System.out.println(x);
+		System.out.println(x--);
+		System.out.println(x);
+
+}}
