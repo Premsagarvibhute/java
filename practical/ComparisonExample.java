bae2d611fd88885259010a85391ae9ab0b71f189
Premsagarvibhute <123161189+Premsagarvibhute@users.noreply.github.com>
2023-01-25 13:40:48 +0530

    Add files via upload

---
+++ practical/ComparisonExample.java
@@ -0,0 +1,18 @@
+class ComparisonExample{
+public void cal(){
+int a=14,b=6;
+System.out.println("a>b:"+(a>b));
+System.out.println("a<b:"+(a<b));
+System.out.println("a>=b:"+(a>=b));
+System.out.println("a<=b:"+(a<=b));
+System.out.println("a==b:"+(a==b));
+System.out.println("a!=b:"+(a!=b));
+System.out.println((a<b)&&(a++>b));
+System.out.println((a>b)&(a++>b));
+System.out.println((a>b)||(a<b));
+
+}
+public static void main(String args[]){
+ComparisonExample obj=new ComparisonExample();
+obj.cal();
+}}
